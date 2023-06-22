5bacc847a8af7a1e83e909fba5cf7445474e125c
Eric Kehoe <ekehoe32@gmail.com>
2023-06-22 01:29:25 -0600

    need forms

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,95 +1,17 @@
-"use client";
+import { RegisterForm } from "./form";
+import Header from "@/components/header";
 
-import { signIn } from "next-auth/react";
-import { ChangeEvent, useState } from "react";
-
-export const RegisterForm = () => {
-  const [loading, setLoading] = useState(false);
-  const [formValues, setFormValues] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-  const [error, setError] = useState("");
-
-  const onSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setLoading(true);
-    setFormValues({ name: "", email: "", password: "" });
-
-    try {
-      const res = await fetch("/api/register", {
-        method: "POST",
-        body: JSON.stringify(formValues),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      setLoading(false);
-      if (!res.ok) {
-        setError((await res.json()).message);
-        return;
-      }
-
-      signIn(undefined, { callbackUrl: "/" });
-    } catch (error: any) {
-      setLoading(false);
-      setError(error);
-    }
-  };
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-
+export default function RegisterPage() {
   return (
-    <form className="prose" onSubmit={onSubmit}>
-      {error && (
-        <p className="text-center bg-red-300 py-4 mb-6 rounded">{error}</p>
-      )}
-      <h1>Register</h1>
-      <div className="mb-6">
-        <input
-          required
-          type="name"
-          name="name"
-          value={formValues.name}
-          onChange={handleChange}
-          placeholder="Name"
-          className="input input-primary w-full"
-        />
-      </div>
-      <div className="mb-6">
-        <input
-          required
-          type="email"
-          name="email"
-          value={formValues.email}
-          onChange={handleChange}
-          placeholder="Email address"
-          className="input input-primary w-full"
-        />
-      </div>
-      <div className="mb-6">
-        <input
-          required
-          type="password"
-          name="password"
-          value={formValues.password}
-          onChange={handleChange}
-          placeholder="Password"
-          className="input input-primary w-full"
-        />
-      </div>
-      <button
-        type="submit"
-        className="btn btn-primary w-full"
-        disabled={loading}
-      >
-        {loading ? "loading..." : "Sign Up"}
-      </button>
-    </form>
+    <>
+      <Header />
+      <section className="min-h-screen pt-20 w-screen">
+        <div className="mx-auto px-6 py-12 h-full flex justify-center items-center">
+          <div className="card md:w-8/12 lg:w-5/12 px-8 py-10 max-w-md">
+            <RegisterForm />
+          </div>
+        </div>
+      </section>
+    </>
   );
-};
+}
--- client/src/components/authLayout.tsx
+++ client/src/components/authLayout.tsx
@@ -1,4 +1,4 @@
-export default function ProfileLayout({
+export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
